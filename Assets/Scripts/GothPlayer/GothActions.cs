3f16b7d372f569131aeae43b4fe4081151166ccc
saikuru0 <NO_EMAIL>
2023-07-19 16:49:08 +0200

    moved GothActions to Player

--- Assets/Scripts/GothPlayer/GothActions.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class GothActions : MonoBehaviour {
-    [SerializeField] private bool isWorking = false;
-    public void UpdateActions(bool work) { isWorking = work; }
-    public bool IsWorking() { return isWorking; }
-}
